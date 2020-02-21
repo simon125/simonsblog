608e0eef2234c3d36d7e0e5ebffb720c6de1fc6d
Szymon <szymqwerty@gmail.com>
2020-02-21 01:09:23 +0100

    Build

--- config.js
+++ config.js
@@ -1,17 +1,18 @@
 module.exports = {
-  title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
-  description: 'Full-stack Web Developer',
+  title: 'MrFrontmen', // Required
+  author: 'Szymon Oleszek', // Required
+  description: 'Full-stack JavaScript developer',
   primaryColor: '#3498db', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
+    // website: 'https://ryanfitzgerald.ca',
+    // facebook: 'https://www.facebook.com/szymon.piotr.oleszek',
+    github: 'https://github.com/simon125',
+    // twitter: 'https://twitter.com/ryanafitzgerald',
+    linkedin: 'https://www.linkedin.com/in/szymon-oleszek/'
   },
   pathPrefix: '/devblog',
-  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
+  siteUrl: 'https://mrfrontmen.netlify.com/'
 };
